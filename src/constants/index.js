de6f5d2f49587421ed457e629fa12c2c6d1b8f55
Om Prakash <ompra.rox@gmail.com>
2023-02-27 15:54:54 +0530

    Navbar added

---
+++ src/constants/index.js
@@ -0,0 +1,26 @@
+export const navLinks = [
+  {
+    id: "skills",
+    title: "Skills & Experience",
+  },
+  {
+    id: "education",
+    title: "Education",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "openSource",
+    title: "Open Source",
+  },
+  {
+    id: "extraCurricular",
+    title: "Extra Curricular",
+  },
+  {
+    id: "contactMe",
+    title: "Contact Me",
+  },
+];
